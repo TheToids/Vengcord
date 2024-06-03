12cf2b519f120851bfa0dbc77d61d48d71a0f7e4
Cody 'Toid <cjdowney@gmail.com>
2024-06-03 15:43:01 -0400

    Remove .env from tracking

--- Vengcord.js
+++ Vengcord.js
@@ -3,57 +3,74 @@ const config = require('./config.json');
 const embedsHS = require('./embeds.json');
 const metricsList = require('./metrics.json');
 const getWebhookIdAndTokenFromLink = (link) => {
-    const regex = /https:\/\/discord\.com\/api\/webhooks\/(\d+)\/([\w-]+)/;
-    const matches = link.match(regex);
-    return matches ? { id: matches[1], token: matches[2] } : null;
-  };
+  const regex = /https:\/\/discord\.com\/api\/webhooks\/(\d+)\/([\w-]+)/;
+  const matches = link.match(regex);
+  return matches ? { id: matches[1], token: matches[2] } : null;
+};
 const getChannelIdFromLink = (link) => {
-    const regex = /https:\/\/discord\.com\/channels\/\d+\/\d+\/(\d+)/;
-    const matches = link.match(regex);
-    return matches ? matches[1] : null;
+  const regex = /https:\/\/discord\.com\/channels\/\d+\/\d+\/(\d+)/;
+  const matches = link.match(regex);
+  return matches ? matches[1] : null;
 };
 const webhookDetails = getWebhookIdAndTokenFromLink(config.webhookUrl);
 const channelId = getChannelIdFromLink(config.previousMessageUrl);
 const webhookClient = new WebhookClient({ id: webhookDetails.id, token: webhookDetails.token });
 
-function replaceTitle(cat, metric){
-  return metricsList.metric.display;
+function replaceTitle(metric) { //clue scrolls all
+  let icon = metricsList.activities[metric].icon;
+  let cat = findParentObject(metricsList, metric);
+  return `**${icon} ${metricsList[cat][metric].display} ${icon}**`;
 }
 
-
-  const embeds = Object.keys(embedsHS.embeds).flatMap(metric => {
-    let metricEmbeds = []; // Create an array to hold the embeds for this metric
-    const playerEntries = Object.entries(embedsHS.embeds[metric]); // Get the entries for the players
-  
-    for (let i = 0; i < playerEntries.length; i += 5) {
-      const playerChunk = playerEntries.slice(i, i + 5); // Slice the array to get the next 5 players
-      // Combine nameValues and xpValues into single strings separated by newlines
-      const nameValues = playerChunk.map(([key, players]) => players.nameValue).join('\n');
-      const xpValues = playerChunk.map(([key, players]) => players.xpValue).join('\n');
-  
-      const embed = new EmbedBuilder()
-        .addFields(
-         
-          { name: `**${metric}**`, value: `${"".padEnd(20, `-`)}`, inline: false },
-          { name: nameValues, value: '\u200B', inline: true },
-          { name: xpValues, value: '\u200B', inline: true },
-          { name: '\u200B', value: '\u200B', inline: true }
-        );
-      metricEmbeds.push(embed); // Add the embed to the array of metric embeds
+function findParentObject(obj, targetKey) {
+  for (let key in obj) {
+    if (obj.hasOwnProperty(key) && typeof obj[key] === 'object') {
+      if (obj[key].hasOwnProperty(targetKey)) {
+        return key; // This is the parent key
+      }
     }
-    return metricEmbeds;
-  });
+  }
+  return null; // If the targetKey wasn't found in any object
+}
 
+const embeds = Object.keys(embedsHS.embeds).flatMap(metric => {
+  let metricEmbeds = []; // Array to hold the embeds for this metric
+  const playerEntries = Object.entries(embedsHS.embeds[metric]); // Entries for the players
 
+  for (let i = 0; i < playerEntries.length; i += 5) {
+    const playerChunk = playerEntries.slice(i, i + 5); // Next 5 players
+    const nameValues = playerChunk.map(([key, players]) => players.nameValue).join('\n');
+    const xpValues = playerChunk.map(([key, players]) => players.xpValue).join('\n');
 
+    const embed = new EmbedBuilder()
+      .addFields(
+        { name: replaceTitle(metric), value: `${"".padEnd(41, `=`)}`, inline: false },
+        { name: nameValues, value: '\u200B', inline: true },
+        { name: '\u200B', value: '\u200B', inline: true },
+        { name: xpValues, value: '\u200B', inline: true }
+        // Removed the third inline field with zero-width space
+      );
+    metricEmbeds.push(embed); // Add the embed to the array
+  }
+  return metricEmbeds;
+});
+
+// Send the embeds in batches of 10 to avoid exceeding the limit
+const createEmbedBatch = async () => {
+  for (let i = 0; i < embeds.length; i += 10) {
+    const embedBatch = embeds.slice(i, i + 10);
+    sendWebhook(embedBatch);
+    //message.channel.send({ embeds: embedBatch });
+  }
+}
 
 // Function to send a new webhook message with an embed
-const sendWebhook = async () => {
+const sendWebhook = async (embeds) => {
   try {
     await webhookClient.send({
-        username: 'Vengienz Hiscores',
-        avatarURL: 'https://i.imgur.com/K0Bz4ZN.png',
-        embeds: embeds
+      username: 'Vengienz Hiscores',
+      avatarURL: 'https://i.imgur.com/K0Bz4ZN.png',
+      embeds: embeds
     });
     console.log('Embed sent successfully');
   } catch (error) {
@@ -65,9 +82,9 @@ const sendWebhook = async () => {
 const updateWebhook = async () => {
   try {
     await webhookClient.editMessage(channelId, {
-        username: 'Vengienz Hiscores',
-        avatarURL: 'https://i.imgur.com/K0Bz4ZN.png',
-        embeds: [embed2]
+      username: 'Vengienz Hiscores',
+      avatarURL: 'https://i.imgur.com/K0Bz4ZN.png',
+      embeds: [embed2]
     });
     console.log('Embed updated successfully');
   } catch (error) {
@@ -83,4 +100,5 @@ const updateWebhook = async () => {
 
 // Call the updateWebhook function to attempt to update the previous message
 //updateWebhook();
-sendWebhook();
+//sendWebhook();
+createEmbedBatch();
